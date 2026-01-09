d82eb8b16a2159a75f8cf615d193fb4fd4c858bf
Alluns <allan.backman@gmail.com>
2026-01-09 15:23:28 +0100

    Made changes 2

--- Assets/Boids.cs
+++ Assets/Boids.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -7,56 +5,75 @@ public class Boids : MonoBehaviour
 {
     [Header("Container")]
     [SerializeField] private Vector3 bounds = Vector3.one;
-    [Range(0, 200)]
+    [Range(0, 3000)]
     [SerializeField] private int boidsAmount;
+
+    [Header("Boids")]
+    [SerializeField] private float contextRadius = 1f;
+    [SerializeField] private float avoidanceRadius = 0.5f;
+    [SerializeField] private float speed = 1f;
+
+
+    [Range(0, 1)] [SerializeField] private float cohesionWeight = 0.25f;
+    [Range(0, 1)] [SerializeField] private float avoidanceWeight = 0.25f;
+    [Range(0, 1)] [SerializeField] private float alignmentWeight = 0.25f;
     
-    private readonly Boid[] boids = new Boid[200];
+    private readonly Boid[] boids = new Boid[3000];
 
     public class Boid
     {
         public Vector3 Position;
-        public Vector3 Direction;
+        public Vector3 Velocity;
         public bool IsEnabled = false;
 
-        private const float TurnRate = 90f;
-        private const float ContextRadius = 1f;
-
-        public void Simulate(Boid[] boids)
+        public void Simulate(Boids container)
         {
-            List<Boid> neighbours = new List<Boid>();
             
-            foreach (Boid boid in boids)
+            Vector3 neighboursCenterOfMass = new();
+            Vector3 closeNeighboursVectors = new();
+            Vector3 neighboursAverageVelocity = new();
+            
+            int neighbourCount = 0;
+            
+            foreach (Boid boid in container.boids)
             {
                 if (!boid.IsEnabled) break;
+                if (Vector3.Distance(Position, boid.Position) > container.contextRadius || boid == this) continue;
+                
+                // Rule 1: Boids try to fly towards the center of mass of its neighbours
+                neighboursCenterOfMass += boid.Position;
 
-                if (Vector3.Distance(Position, boid.Position) < ContextRadius && boid != this)
+                // Rule 2: Boids try to keep a small distance from the neighbours
+                if (Vector3.Distance(Position, boid.Position) < container.avoidanceRadius)
                 {
-                    neighbours.Add(boid);
+                    closeNeighboursVectors -= boid.Position - Position;
                 }
+                
+                // Rule 3: Boids try to match the direction (velocity?) of its neighbours
+                neighboursAverageVelocity += boid.Velocity;
+                
+                neighbourCount++;
             }
-
-            if (neighbours.Count > 0)
-            {
-                // separation: steer to avoid crowding local flockmates
-                //TODO: Steer away from the average point of all neighbours
+            neighboursCenterOfMass /= neighbourCount;
+            neighboursAverageVelocity /= neighbourCount;
             
-                // alignment: steer towards the average heading of local flockmates
-                //TODO: Steer towards the average direction of all neighbours
+            Vector3 cohesion = (neighboursCenterOfMass - Position).normalized * container.cohesionWeight;
+            Vector3 avoidance = closeNeighboursVectors.normalized * container.avoidanceWeight;
+            Vector3 alignment = neighboursAverageVelocity.normalized * container.alignmentWeight;
             
-                // cohesion: steer to move towards the average position (center of mass) of local flockmates
-                //TODO: Steer towards the average point of all neighbours
-                // Vector3 averagePosition = new();
-                //
-                // foreach (Boid neighbour in neighbours)
-                // {
-                //     averagePosition += neighbour.Position;
-                // }
-                // averagePosition /= neighbours.Count;
-                //
-                // Direction = Vector3.RotateTowards(Direction, averagePosition, TurnRate * Mathf.Deg2Rad * Time.deltaTime, 0.0f);
-            }
+            Velocity = (Velocity.normalized + cohesion + avoidance + alignment) * container.speed;
+            Position += Velocity * Time.deltaTime ;
             
-            Position += Direction * Time.deltaTime;
+            Position = new Vector3(
+                Position.x = Position.x > 0 ?
+                    (Position.x + container.bounds.x / 2) % container.bounds.x - container.bounds.x / 2 :
+                    (Position.x - container.bounds.x / 2) % container.bounds.x + container.bounds.x / 2,
+                Position.y = Position.y > 0 ?
+                    (Position.y + container.bounds.y / 2) % container.bounds.y - container.bounds.y / 2 :
+                    (Position.y - container.bounds.y / 2) % container.bounds.y + container.bounds.y / 2,
+                Position.z = Position.z > 0 ?
+                    (Position.z + container.bounds.z / 2) % container.bounds.z - container.bounds.z / 2 :
+                    (Position.z - container.bounds.z / 2) % container.bounds.z + container.bounds.z / 2);
         }
     }
 
@@ -67,7 +84,7 @@ public class Boids : MonoBehaviour
             boids[i] = new Boid
             {
                 Position = new Vector3(Random.Range(0, bounds.x) - bounds.x / 2, Random.Range(0, bounds.y) - bounds.y / 2, Random.Range(0, bounds.z) - bounds.z / 2),
-                Direction = Vector3.forward,
+                Velocity = Vector3.forward,
                 IsEnabled = i < boidsAmount
             };
         }
@@ -78,12 +95,14 @@ public class Boids : MonoBehaviour
         foreach (Boid boid in boids)
         {
             if (!boid.IsEnabled) break;
-            boid.Simulate(boids);
+            boid.Simulate(this);
         }
     }
-
+    
     private void OnValidate()
     {
+        if (!Application.isPlaying) return;
+        
         for (int i = 0; i < boids.Length; i++)
         {
             boids[i].IsEnabled = i < boidsAmount;
@@ -93,23 +112,24 @@ public class Boids : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        
         // Bounding box
         Gizmos.color = Color.white;
         Gizmos.DrawWireCube(transform.position, bounds);
-
+        
         // Boids
         foreach (Boid boid in boids)
         {
             if (boid is not { IsEnabled: true }) break;
             
-            Gizmos.color = Color.dodgerBlue;
-            Gizmos.DrawSphere(boid.Position, 0.05f);
+            Gizmos.color = Color.Lerp(Color.darkGreen, Color.limeGreen, Mathf.InverseLerp(-bounds.y / 2, bounds.y / 2, boid.Position.y));
+            Gizmos.DrawSphere(boid.Position, 0.1f);
             
-            Gizmos.color = Color.darkOrange;
-            Gizmos.DrawWireSphere(boid.Position, 1f);
+            // Gizmos.color = Color.darkOrange;
+            // Gizmos.DrawWireSphere(boid.Position, contextRadius);
             
-            Gizmos.color = Color.limeGreen;
-            Gizmos.DrawRay(new Ray(boid.Position, boid.Direction));
+            // Gizmos.color = Color.limeGreen;
+            // Gizmos.DrawRay(new Ray(boid.Position, boid.Velocity));
         }
     }
 }
